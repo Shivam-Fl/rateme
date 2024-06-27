03049fa6502676c0067a357d7908d3bc6ae3239d
Shivam Mishra <126186920+Shivam-Fl@users.noreply.github.com>
2024-06-27 20:55:43 +0530

    Update Navbar.jsx

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -207,7 +207,7 @@ function Navbar() {
               ) : (
                 <img 
           
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Ic_arrow_drop_down_36px.svg/1200px-Ic_arrow_drop_down_36px.svg.png" className="w-10 h-10"/>
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Ic_arrow_drop_down_36px.svg/1200px-Ic_arrow_drop_down_36px.svg.png" className="md:w-10 md:h-10 w-8 h-8"/>
               )}
             </button>
           </div>
